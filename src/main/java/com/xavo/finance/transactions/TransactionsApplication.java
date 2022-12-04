775e348a6625ca2bdab4d8dd9cc57c462cfe6a13
riruiz <riruiz@host.docker.internal>
2022-12-03 21:47:07 -0600

    Remove eureka dependencies

--- src/main/java/com/xavo/finance/transactions/TransactionsApplication.java
+++ src/main/java/com/xavo/finance/transactions/TransactionsApplication.java
@@ -3,13 +3,11 @@ package com.xavo.finance.transactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class TransactionsApplication {
 
 	public static void main(String[] args) {
